3b473403b9fefcd603343926c83da9078abbb1fa
maijz128 <maijz128@gmail.com>
2019-04-16 21:41:39 +0800

    重构英雄技能：狼人 技能：解除变身

--- Ability/heroes/lycan/lycan_shapeshift_relieve/game/scripts/vscripts/abilities/hero_lycan/mjz_lycan_shapeshift_relieve.lua
+++ Ability/heroes/lycan/lycan_shapeshift_relieve/game/scripts/vscripts/abilities/hero_lycan/mjz_lycan_shapeshift_relieve.lua
@@ -1,81 +1,134 @@
 
-function OnSpellStart( event )
-	if not IsServer() then return nil end
+local THIS_LUA = "abilities/hero_lycan/mjz_lycan_shapeshift_relieve.lua"
+local MODIFIER_RELIEVE_NAME = 'modifier_mjz_lycan_shapeshift_relieve'
+local MODIFIER_HEALTH_NAME = 'modifier_mjz_lycan_shapeshift_relieve_health'
 
-	local caster = event.caster
-	local ability = event.ability
+local TARGET_MODIFIER_LIST = {
+	'modifier_lycan_shapeshift',
+	'modifier_lycan_shapeshift_aura',
+	'modifier_lycan_shapeshift_speed',
+}
+
+LinkLuaModifier(MODIFIER_RELIEVE_NAME, THIS_LUA, LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier(MODIFIER_HEALTH_NAME, THIS_LUA, LUA_MODIFIER_MOTION_NONE)
+
+------------------------------------------------------------------------------------------
+
+mjz_lycan_shapeshift_relieve = class({})
+local ability_class = mjz_lycan_shapeshift_relieve
+
+function ability_class:GetIntrinsicModifierName()
+    return MODIFIER_RELIEVE_NAME
+end
+
+if IsServer() then
+	function ability_class:OnSpellStart(  )
+		local caster = self:GetCaster()
+		local ability = self
+		local unit = caster
 	
-	local unit = caster
-	local modifier_health_name = 'modifier_mjz_lycan_shapeshift_relieve_health'
-
-	local modifier_shapeshift = unit:FindModifierByName('modifier_lycan_shapeshift')
-	local modifier_shapeshift_aura = unit:FindModifierByName('modifier_lycan_shapeshift_aura')
-	local modifier_shapeshift_speed = unit:FindModifierByName('modifier_lycan_shapeshift_speed')
-
-	if  modifier_shapeshift then
-		modifier_shapeshift:SetDuration(0.1, false)
-		if modifier_shapeshift_aura then
-			modifier_shapeshift_aura:SetDuration(0.1, false)
-		end
-
-		if unit:HasModifier(modifier_health_name) then
-			unit:RemoveModifierByName(modifier_health_name)	
+		if HasModifierList(unit, TARGET_MODIFIER_LIST) then
+			SetModifierListDuration(unit, TARGET_MODIFIER_LIST, 0.1)
+	
+			if unit:HasModifier(MODIFIER_HEALTH_NAME) then
+				unit:RemoveModifierByName(MODIFIER_HEALTH_NAME)	
+			end
 		end
 	end
 end
 
-function OnCreated( event )
-	if not IsServer() then return nil end
 
-	local caster = event.caster
-	local ability = event.ability
+------------------------------------------------------------------------------------------
 
-end
 
-function OnIntervalThink( event )
-	if not IsServer() then return nil end
+modifier_mjz_lycan_shapeshift_relieve = class({})
+local modifier_relieve = modifier_mjz_lycan_shapeshift_relieve
 
-	local caster = event.caster
-	local ability = event.ability
-	
-	local unit = caster
-	local modifier_health_name = 'modifier_mjz_lycan_shapeshift_relieve_health'
+function modifier_relieve:IsHidden() return true end
+function modifier_relieve:IsPurgable() return false end
 
-	local modifier_shapeshift = unit:FindModifierByName('modifier_lycan_shapeshift')
-	local modifier_shapeshift_aura = unit:FindModifierByName('modifier_lycan_shapeshift_aura')
-	local modifier_shapeshift_speed = unit:FindModifierByName('modifier_lycan_shapeshift_speed')
+if IsServer() then
+	function modifier_relieve:OnCreated(table)
+		local caster = self:GetCaster()
+		local parent = self:GetParent()
+		local ability = self:GetAbility()
 
-	if unit:HasScepter() and modifier_shapeshift then
-		modifier_shapeshift:SetDuration(20.0, false)
-		if modifier_shapeshift_aura then
-			modifier_shapeshift_aura:SetDuration(20.0, false)
-		end
-
-		if not unit:HasModifier(modifier_health_name) then
-			unit:AddNewModifier(unit, ability, modifier_health_name, {})
-			-- ability:ApplyDataDrivenModifier(ability, unit, modifier_health_name, {})
-		end
-	else
-		unit:RemoveModifierByName(modifier_health_name)	
+		local think_interval = ability:GetSpecialValueFor('think_interval')
+		self:StartIntervalThink(think_interval)
 	end
+
+	function modifier_relieve:OnIntervalThink()
+		local caster = self:GetCaster()
+		local ability = self:GetAbility()
+		local unit = self:GetParent()
+
+		if unit:HasScepter() and self:InShapeshift() then
+			self:UpdateDuration()
+			if not unit:HasModifier(MODIFIER_HEALTH_NAME) then
+				unit:AddNewModifier(caster, ability, MODIFIER_HEALTH_NAME, {})
+			end
+		else
+			unit:RemoveModifierByName(MODIFIER_HEALTH_NAME)	
+		end
+	end
+	
+	function modifier_relieve:InShapeshift( )
+		local ability = self:GetAbility()
+		local unit = self:GetParent()
+		return HasModifierList(unit, TARGET_MODIFIER_LIST)
+	end
+
+	function modifier_relieve:UpdateDuration( )
+		local ability = self:GetAbility()
+		local unit = self:GetParent()
+		local duration = ability:GetSpecialValueFor('duration_scepter')
+
+		SetModifierListDuration(unit, TARGET_MODIFIER_LIST, duration)
+	end
+
 end
 
---------------------------------------------------------------------------------------------
-
-LinkLuaModifier("modifier_mjz_lycan_shapeshift_relieve_health", "abilities/hero_lycan/mjz_lycan_shapeshift_relieve.lua", LUA_MODIFIER_MOTION_NONE)
+------------------------------------------------------------------------------------------
 
 modifier_mjz_lycan_shapeshift_relieve_health = class({})
-function modifier_mjz_lycan_shapeshift_relieve_health:IsHidden()
-    return true
-end
-function modifier_mjz_lycan_shapeshift_relieve_health:DeclareFunctions()
-    local funcs = {
+local modifier_health = modifier_mjz_lycan_shapeshift_relieve_health
+
+function modifier_health:IsHidden() return true end
+function modifier_health:IsPurgable() return false end
+
+function modifier_health:DeclareFunctions()
+    return {
         MODIFIER_PROPERTY_HEALTH_REGEN_PERCENTAGE
     }
-    return funcs
 end
-function modifier_mjz_lycan_shapeshift_relieve_health:GetModifierHealthRegenPercentage( params )
-	return -1.0
+function modifier_health:GetModifierHealthRegenPercentage( params )
+	return self:GetAbility():GetSpecialValueFor('health_drain_per_second_scepter')
 end
 
 
+------------------------------------------------------------------------------------------
+
+function HasModifierList( unit, modifier_name_list )
+	for _,modifier_name in pairs(modifier_name_list) do
+		if not unit:HasModifier(modifier_name) then
+			return false
+		end
+	end
+	return true
+end
+
+function SetModifierListDuration( unit, modifier_name_list, duration )
+	for _,modifier_name in pairs(modifier_name_list) do
+		local modifier = unit:FindModifierByName(modifier_name)
+		if modifier then
+			modifier:SetDuration(duration, true)
+		end
+	end
+end
+
+function SetModifierDuration( unit, modifier_name, duration )
+	local modifier = unit:FindModifierByName(modifier_name)
+	if modifier then
+		modifier:SetDuration(duration, true)
+	end
+end
