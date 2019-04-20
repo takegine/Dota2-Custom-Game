14e45ca9a2dc169385569dbfea0a2b9d6a67c0c3
maijz128 <maijz128@gmail.com>
2019-04-20 20:21:48 +0800

    重构英雄技能：哈斯卡 技能：火矛

--- Ability/heroes/huskar/huskar_burning_spear_frostivus2018/game/scripts/vscripts/abilities/hero_huskar/mjz_huskar_burning_spear_frostivus2018.lua
+++ Ability/heroes/huskar/huskar_burning_spear_frostivus2018/game/scripts/vscripts/abilities/hero_huskar/mjz_huskar_burning_spear_frostivus2018.lua
@@ -1,27 +1,109 @@
 
+local THIS_LUA = "abilities/hero_huskar/mjz_huskar_burning_spear_frostivus2018.lua"
+local MODIFIER_LUA = "modifiers/hero_huskar/modifier_mjz_huskar_burning_spear_frostivus2018.lua"
+local MODIFIER_COUNTER_NAME = 'modifier_mjz_huskar_burning_spear_frostivus2018_debuff'
+local MODIFIER_EFFECT_NAME = 'modifier_mjz_huskar_burning_spear_frostivus2018_debuff_effect'
+local MODIFIER_TALENT_NAME = 'modifier_special_bonus_unique_huskar_5'
+local TALENT_NAME = 'special_bonus_unique_huskar_5'
+
+LinkLuaModifier(MODIFIER_COUNTER_NAME, MODIFIER_LUA, LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier(MODIFIER_EFFECT_NAME, MODIFIER_LUA, LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier(MODIFIER_TALENT_NAME, MODIFIER_LUA, LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_generic_orb_effect_v1_0", "modifiers/modifier_generic_orb_effect_v1.0.lua", LUA_MODIFIER_MOTION_NONE)
+
+---------------------------------------------------------------------------------------
+
+mjz_huskar_burning_spear_frostivus2018 = class({})
+local ability_class = mjz_huskar_burning_spear_frostivus2018
+
+function ability_class:IsRefreshable() return true end
+function ability_class:IsStealable() return false end	-- 是否可以被法术偷取。
+
+function ability_class:GetAbilityDamageType()
+    if self:GetCaster():HasModifier(MODIFIER_TALENT_NAME) then
+        return DAMAGE_TYPE_PURE
+    else
+        return DAMAGE_TYPE_MAGICAL
+    end
+end
+
+
+function ability_class:GetAbilityTargetFlags( )
+    if self:GetCaster():HasModifier(MODIFIER_TALENT_NAME) then
+        return DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES
+    else
+        return DOTA_UNIT_TARGET_FLAG_NONE
+    end
+end
+
+--------------------------------------------------------------------------------
+function ability_class:GetIntrinsicModifierName()
+    return "modifier_generic_orb_effect_v1_0"
+end
+
+function ability_class:OnAbilityPhaseStart()
+
+end
+
+-- Orb Effects
+function ability_class:GetProjectileName()
+	return "particles/units/heroes/hero_huskar/huskar_burning_spear.vpcf"
+end
+
+function ability_class:OnOrbFire( params )
+	local ability = self
+	local caster = self:GetCaster()
+	local target = params.target
+    -- play effects
+    
+    if HasTalent(caster, TALENT_NAME) then
+        if not caster:HasModifier(MODIFIER_TALENT_NAME) then
+            caster:AddNewModifier(caster, ability, MODIFIER_TALENT_NAME, {})
+        end
+    end
+    
+    DoHealthCost({
+        caster = caster,
+        ability = ability,
+    })
+end
+
+function ability_class:OnOrbImpact( params )
+	local ability = self
+	local caster = self:GetCaster()
+	local target = params.target
+
+	OnOrbImpact({
+        caster = caster,
+        ability = ability,
+        target = target,
+    })
+end
+
+--------------------------------------------------------------------------------
 
 function OnOrbImpact( keys )
-    if not IsServer() then return nil end
 
 	local caster = keys.caster
 	local ability = keys.ability
 	local target = keys.target
-	local modifier_name = keys.modifier_name
-    local modifier_counter_name = keys.modifier_counter_name
     
 	local spear_aoe = ability:GetLevelSpecialValueFor( "spear_aoe" , ability:GetLevel() - 1 )
 	local duration = ability:GetLevelSpecialValueFor( "burn_duration" , ability:GetLevel() - 1 )
-    
-	local enemy_list = FindTargetEnemy(caster, target:GetAbsOrigin(), spear_aoe)
+    local flags = ability:GetAbilityTargetFlags()
+    -- local enemy_list = FindTargetEnemy(caster, target:GetAbsOrigin(), spear_aoe)
+    local enemy_list = FindTargetEnemy_Flags(caster, target:GetAbsOrigin(), spear_aoe, flags)
     for _,enemy in pairs(enemy_list) do
 
         IncreaseModifierStackCount({
             caster = caster, ability = ability, target = enemy,
-            modifier_name = modifier_counter_name, duration = duration
+            modifier_name = MODIFIER_COUNTER_NAME, duration = duration
         })
 
-        ability:ApplyDataDrivenModifier(caster, enemy, modifier_name, {duration = duration})		
-	end
+        enemy:AddNewModifier(caster, ability, MODIFIER_EFFECT_NAME, {duration = duration})
+    end
+    
+    EmitSoundOn("Hero_Huskar.Burning_Spear", target)
 end
 
 
@@ -40,6 +122,8 @@ function DoHealthCost( event )
     -- aka set the casters HP to the new value
     -- ModifyHealth's third parameter lets us decide if the healthcost should be lethal
     caster:ModifyHealth(new_health, ability, false, 0)
+
+    EmitSoundOn("Hero_Huskar.Burning_Spear.Cast", caster)
 end
 
 function IncreaseModifierStackCount( kv )
@@ -56,60 +140,12 @@ function IncreaseModifierStackCount( kv )
         target:SetModifierStackCount(modifier_name, caster, count + 1)
         modifier:SetDuration(duration, true)
     else
-        ability:ApplyDataDrivenModifier(caster, target, modifier_name, { duration = duration })
+        target:AddNewModifier(caster, ability, modifier_name, {duration = duration})
         target:SetModifierStackCount(modifier_name, caster, 1) 
     end
 end
 
-
---[[
-    Increases stack count on the visual modifier
-    This is called everytime an enemy is hit by burning_spear
-]]
-function IncreaseStackCount( event )
-    local caster = event.caster
-    local target = event.target
-    local ability = event.ability
-    local modifier_name = event.modifier_counter_name
-    local dur = ability:GetLevelSpecialValueFor( "burn_duration" , ability:GetLevel() - 1 )
-
-    local modifier = target:FindModifierByName(modifier_name)
-    local count = target:GetModifierStackCount(modifier_name, caster)
-
-    -- if the unit does not already have the counter modifier we apply it with a stackcount of 1
-    -- else we increase the stack and refresh the counters duration
-    if not modifier then
-        ability:ApplyDataDrivenModifier(caster, target, modifier_name, {duration=dur})
-        target:SetModifierStackCount(modifier_name, caster, 1) 
-    else
-        target:SetModifierStackCount(modifier_name, caster, count+1)
-        modifier:SetDuration(dur, true)
-    end
-end
-
---[[
-    Decreases stack count on the visual modifier 
-    This is called whenever the debuff modifier runs out
-]]
-function DecreaseStackCount(event)
-    local caster = event.caster
-    local target = event.target
-    local modifier_name = event.modifier_counter_name
-    local modifier = target:FindModifierByName(modifier_name)
-    local count = target:GetModifierStackCount(modifier_name, caster)
-
-    -- just some saftey checks -just in case
-    if modifier then
-
-        -- if there is something to reduce reduce
-        -- else just remove the modifier
-        if count and count > 1 then
-            target:SetModifierStackCount(modifier_name, caster, count-1)
-        else
-            target:RemoveModifierByName(modifier_name)
-        end
-    end
-end
+---------------------------------------------------------------------------------------
 
 -- 搜索目标位置所有的敌人单位
 function FindTargetEnemy(unit, point, radius)
@@ -125,4 +161,50 @@ function FindTargetEnemy(unit, point, radius)
     local bCanGrowCache = false             -- 通常值
     return FindUnitsInRadius( iTeamNumber, vPosition, hCacheUnit, 
         flRadius, iTeamFilter, iTypeFilter, iFlagFilter, iOrder, bCanGrowCache )
+end
+
+-- 搜索目标位置所有的敌人单位
+function FindTargetEnemy_Flags(unit, point, radius, flags)
+    local iTeamNumber = unit:GetTeamNumber()
+    local vPosition = point   				-- 搜索中心点
+    local hCacheUnit = nil                  -- 通常值
+    local flRadius = radius                 -- 搜索范围
+    local iTeamFilter = DOTA_UNIT_TARGET_TEAM_ENEMY  -- 目标是敌人单位
+    -- 目标单位类型
+    local iTypeFilter = DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC --DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_CREEP           
+    local iFlagFilter = flags
+    local iOrder = FIND_CLOSEST                         -- 寻找最近的
+    local bCanGrowCache = false             -- 通常值
+    return FindUnitsInRadius( iTeamNumber, vPosition, hCacheUnit, 
+        flRadius, iTeamFilter, iTypeFilter, iFlagFilter, iOrder, bCanGrowCache )
+end
+
+
+-- 是否学习了指定天赋技能
+function HasTalent(unit, talentName)
+    if unit:HasAbility(talentName) then
+        if unit:FindAbilityByName(talentName):GetLevel() > 0 then return true end
+    end
+    return false
+end
+
+-- 获得技能数据中的数据值，如果学习了连接的天赋技能，就返回相加结果
+function GetTalentSpecialValueFor(ability, value)
+    local base = ability:GetSpecialValueFor(value)
+    local talentName
+    local kv = ability:GetAbilityKeyValues()
+    for k,v in pairs(kv) do -- trawl through keyvalues
+        if k == "AbilitySpecial" then
+            for l,m in pairs(v) do
+                if m[value] then
+                    talentName = m["LinkedSpecialBonus"]
+                end
+            end
+        end
+    end
+    if talentName then 
+        local talent = ability:GetCaster():FindAbilityByName(talentName)
+        if talent and talent:GetLevel() > 0 then base = base + talent:GetSpecialValueFor("value") end
+    end
+    return base
 end
