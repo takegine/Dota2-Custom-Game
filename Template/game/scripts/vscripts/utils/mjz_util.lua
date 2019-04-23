135da52311cee81324d1b769ffe52067aadd2ad7
maijz128 <maijz128@gmail.com>
2019-04-23 20:52:29 +0800

    更新地图模版

--- Template/game/scripts/vscripts/utils/mjz_util.lua
+++ Template/game/scripts/vscripts/utils/mjz_util.lua
@@ -101,3 +101,107 @@ function CreateUnit_DummyTarget(point, team, playerID)
   return hDummy
 end
 
+
+
+function TargetIsFriendly(caster, target )
+	local nTargetTeam = DOTA_UNIT_TARGET_TEAM_FRIENDLY 	-- ability:GetAbilityTargetTeam()
+	local nTargetType = DOTA_UNIT_TARGET_ALL 			-- ability:GetAbilityTargetType()
+	local nTargetFlags = DOTA_UNIT_TARGET_FLAG_NONE		-- ability:GetAbilityTargetFlags()
+	local nTeam = caster:GetTeamNumber()
+	local ufResult = UnitFilter(target, nTargetTeam, nTargetType, nTargetFlags, nTeam)
+	return ufResult == UF_SUCCESS
+end
+
+function TargetIsEnemy(caster, target )
+	local nTargetTeam = DOTA_UNIT_TARGET_TEAM_ENEMY 	-- ability:GetAbilityTargetTeam()
+	local nTargetType = DOTA_UNIT_TARGET_ALL 			-- ability:GetAbilityTargetType()
+	local nTargetFlags = DOTA_UNIT_TARGET_FLAG_NONE		-- ability:GetAbilityTargetFlags()
+	local nTeam = caster:GetTeamNumber()
+	local ufResult = UnitFilter(target, nTargetTeam, nTargetType, nTargetFlags, nTeam)
+	return ufResult == UF_SUCCESS
+end
+
+
+--[[
+    A quick function to create popups.
+    Example:
+    create_popup({
+        target = target,
+        value = value,
+        color = Vector(255, 20, 147),
+        type = "spell_custom"
+	}) 
+	伤害类型的颜色：
+		物理：Vector(174, 47, 40)
+		魔法：Vector(91, 147, 209)
+		纯粹：Vector(216, 174, 83)
+	spell_custom: 
+		block | crit | damage | evade | gold | heal | mana_add | mana_loss | miss | poison | spell | xp
+	Color:	
+		red 	={255,0,0},
+		orange	={255,127,0},
+		yellow	={255,255,0},
+		green 	={0,255,0},
+		blue 	={0,0,255},
+		indigo 	={0,255,255},
+		purple 	={255,0,255},
+]]
+function create_popup(data)
+    local target = data.target
+    local value = math.floor(data.value)
+
+    local type = data.type or "miss"
+    local color = data.color or Vector(255, 255, 255)
+    local duration = data.duration or 1.0
+
+    local size = string.len(value)
+
+    local pre = data.pre or nil
+    if pre ~= nil then
+        size = size + 1
+    end
+
+    local pos = data.pos or nil
+    if pos ~= nil then
+        size = size + 1
+    end
+
+    local particle_path = "particles/msg_fx/msg_" .. type .. ".vpcf"
+    local particle = ParticleManager:CreateParticle(particle_path, PATTACH_OVERHEAD_FOLLOW, target)
+    ParticleManager:SetParticleControl(particle, 1, Vector(pre, value, pos))
+    ParticleManager:SetParticleControl(particle, 2, Vector(duration, size, 0))
+    ParticleManager:SetParticleControl(particle, 3, color)
+	ParticleManager:ReleaseParticleIndex(particle)
+end
+
+
+
+-- 是否学习了指定天赋技能
+function HasTalent(unit, talentName)
+    if unit:HasAbility(talentName) then
+        if unit:FindAbilityByName(talentName):GetLevel() > 0 then return true end
+    end
+    return false
+end
+ 
+-- 获得技能数据中的数据值，如果学习了连接的天赋技能，就返回相加结果
+function GetTalentSpecialValueFor(ability, value)
+    local base = ability:GetSpecialValueFor(value)
+    local talentName
+    local kv = ability:GetAbilityKeyValues()
+    for k,v in pairs(kv) do -- trawl through keyvalues
+        if k == "AbilitySpecial" then
+            for l,m in pairs(v) do
+                if m[value] then
+                    talentName = m["LinkedSpecialBonus"]
+                end
+            end
+        end
+    end
+    if talentName then 
+        local talent = ability:GetCaster():FindAbilityByName(talentName)
+        if talent and talent:GetLevel() > 0 then base = base + talent:GetSpecialValueFor("value") end
+    end
+    return base
+end
+
