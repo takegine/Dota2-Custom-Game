01fb587396b22c1ab21d698e492c2230786f575e
maijz128 <maijz128@gmail.com>
2019-05-03 14:03:53 +0800

    更新英雄技能：莉娜 技能：神灭斩 更新：完善技能

--- Ability/heroes/lina/lina_laguna_blade/game/scripts/vscripts/abilities/hero_lina/mjz_lina_laguna_blade.lua
+++ Ability/heroes/lina/lina_laguna_blade/game/scripts/vscripts/abilities/hero_lina/mjz_lina_laguna_blade.lua
@@ -1,5 +1,4 @@
 LinkLuaModifier("modifier_mjz_lina_laguna_blade_bonus", "modifiers/hero_lina/modifier_mjz_lina_laguna_blade.lua", LUA_MODIFIER_MOTION_NONE)
-LinkLuaModifier("modifier_mjz_lina_laguna_blade_creature", "modifiers/hero_lina/modifier_mjz_lina_laguna_blade.lua", LUA_MODIFIER_MOTION_NONE)
 LinkLuaModifier("modifier_mjz_lina_laguna_blade_death", "modifiers/hero_lina/modifier_mjz_lina_laguna_blade.lua", LUA_MODIFIER_MOTION_NONE)
 
 mjz_lina_laguna_blade = class({})
@@ -10,24 +9,30 @@ end
 
 function mjz_lina_laguna_blade:GetAbilityTextureName()
     if self:GetCaster():HasScepter() then
-        -- return "mjz_lina_laguna_blade"
-        return "lina/lina_ti6_immortal/lina_laguna_blade"
+        return "mjz_lina_laguna_blade_ti6_immortal"
     end
-    return "lina_laguna_blade"
-    -- return self.BaseClass.GetAbilityTextureName(self)
+    return "mjz_lina_laguna_blade"
+end
+
+function mjz_lina_laguna_blade:GetBehavior()
+    if self:GetCaster():HasScepter() then
+        return DOTA_ABILITY_BEHAVIOR_UNIT_TARGET + DOTA_ABILITY_BEHAVIOR_AOE
+    end
+    return DOTA_ABILITY_BEHAVIOR_UNIT_TARGET
 end
 
 function mjz_lina_laguna_blade:GetAOERadius()
     if self:GetCaster():HasScepter() then
-        return self:GetSpecialValueFor("cast_range_scepter")
+        return self:GetSpecialValueFor("splash_radius_scepter")
     end
-    return self.BaseClass.GetAOERadius(self)
+    return self:GetSpecialValueFor('cast_range')
 end
+
 function mjz_lina_laguna_blade:GetCastRange(vLocation, hTarget)
     if self:GetCaster():HasScepter() then
         return self:GetSpecialValueFor("cast_range_scepter")
     end
-    return self.BaseClass.GetCastRange(self, vLocation, hTarget)
+    return self:GetSpecialValueFor('cast_range')
 end
 
 function mjz_lina_laguna_blade:GetCooldown(iLevel)
@@ -41,6 +46,13 @@ function mjz_lina_laguna_blade:GetManaCost(iLevel)
         return self:GetSpecialValueFor("mana_cost_scepter")
     end
     return self.BaseClass.GetManaCost(self, iLevel)
+end
+
+function mjz_lina_laguna_blade:GetAbilityTargetFlags()
+    if self:GetCaster():HasScepter() then
+        return DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES
+    end
+    return DOTA_UNIT_TARGET_FLAG_NONE
 end
 
 function mjz_lina_laguna_blade:GetDamageType()
@@ -60,21 +72,10 @@ end
 if IsServer() then
 
     function mjz_lina_laguna_blade:OnSpellStart( )
-        if not IsServer() then return nil end
-
         local caster = self:GetCaster()
         local ability = self
      
-        self.stack_modifier_name = "modifier_mjz_lina_laguna_blade_bonus"
-        self.creature_enabled = self:GetSpecialValueFor("creature_enabled")
-        self.creature_health = self:GetSpecialValueFor("creature_health")
-
-        local base_damage = self:GetSpecialValueFor(value_if_scepter(caster, "damage_scepter", "damage"))
-        local damage_per_kill = self:GetSpecialValueFor("damage_per_kill")
-        local kill_grace_duration = self:GetSpecialValueFor("kill_grace_duration")
-
-        local kill_count = caster:GetModifierStackCount(self.stack_modifier_name, nil)
-        local damage = base_damage + damage_per_kill * kill_count
+        local damage = self:_CalcDamage()
 
         if caster:HasScepter() then
             caster:EmitSound("Hero_Lina.LagunaBlade.Immortal")
@@ -84,24 +85,40 @@ if IsServer() then
 
         local targets = self:_GetTargets()
         for _, target in ipairs(targets) do
-            self:_LagunaBladeTarget(target, damage, kill_grace_duration)
+            self:_PlayEffect(target)
+            self:_AddDeathMonitor(target)
+            self:_ApplyDamage(target, damage)
         end
-        
+    end
+
+    function mjz_lina_laguna_blade:_CalcDamage()
+        local caster = self:GetCaster()
+        local ability = self
+     
+        self.stack_modifier_name = "modifier_mjz_lina_laguna_blade_bonus"
+
+        local base_damage = self:GetSpecialValueFor(value_if_scepter(caster, "damage_scepter", "damage"))
+        local damage_per_kill = self:GetSpecialValueFor("damage_per_kill")
+
+        local kill_count = caster:GetModifierStackCount(self.stack_modifier_name, nil)
+        local damage = base_damage + damage_per_kill * kill_count
+        return damage
     end
 
     function mjz_lina_laguna_blade:_GetTargets()
+        local ability = self
         local caster = self:GetCaster()
         local target = self:GetCursorTarget()
 
         if caster:HasScepter() then
+            local splash_radius = ability:GetSpecialValueFor("splash_radius_scepter")
             return FindUnitsInRadius(
                 caster:GetTeamNumber(),
                 target:GetAbsOrigin(),
-                nil,
-                self:GetSpecialValueFor("splash_radius_scepter"),
-                DOTA_UNIT_TARGET_TEAM_ENEMY,
-                DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC,
-                0,
+                nil, splash_radius,
+                ability:GetAbilityTargetTeam(),
+                ability:GetAbilityTargetType(),
+                DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES,
                 FIND_ANY_ORDER,
                 false
             )
@@ -109,11 +126,10 @@ if IsServer() then
 
         local targets = {}
         table.insert(targets, target)
-
         return targets
     end
 
-    function mjz_lina_laguna_blade:_LagunaBladeTarget(target, damage, kill_grace_duration)
+    function mjz_lina_laguna_blade:_PlayEffect(target )
         local caster = self:GetCaster()
         local ability = self
 
@@ -126,34 +142,63 @@ if IsServer() then
         ParticleManager:SetParticleControl(particle, 1, target:GetAbsOrigin())
         ParticleManager:SetParticleControl(particle, 2, target:GetAbsOrigin())
         ParticleManager:ReleaseParticleIndex(particle)
+    end
+
+    function mjz_lina_laguna_blade:_AddDeathMonitor(target )
+        if target:IsIllusion() then return nil end
+
+        local ability = self
+        local caster = self:GetCaster()
+        local kill_grace_duration = ability:GetSpecialValueFor("kill_grace_duration")
+        local creature_enabled = ability:GetSpecialValueFor("creature_enabled")
+        -- self.creature_health = ability:GetSpecialValueFor("creature_health")
+        local modifier_death = 'modifier_mjz_lina_laguna_blade_death'
+        
+        local can = false
+        if target:IsRealHero() then
+            can = true
+        else
+            if creature_enabled and creature_enabled > 0 then
+                can = true
+            end
+        end
+
+        --添加死亡监听器
+        if can then
+            target:AddNewModifier(caster, ability, modifier_death, {duration=kill_grace_duration})
+        end
+    end
+
+    function mjz_lina_laguna_blade:_ApplyDamage(target, damage)
+        local caster = self:GetCaster()
+        local ability = self
+
+        local sound_default = "Ability.LagunaBladeImpact"
+        local sound_immortal = "Hero_Lina.LagunaBladeImpact.Immortal"
+        local sound_name = value_if_scepter(caster, sound_immortal, sound_default)
+        target:EmitSound(sound_name)
+
+        -- 林肯法球
+        if target:TriggerSpellAbsorb(ability) then return nil end
 
         if not target:IsMagicImmune() or caster:HasScepter() then
-
-            local sound_name = value_if_scepter(caster, "Hero_Lina.LagunaBladeImpact.Immortal", "Ability.LagunaBladeImpact")
-            target:EmitSound(sound_name)
-
-            --添加死亡监听器
-            local victim = target
-            if not victim:IsIllusion() then
-                if victim:IsRealHero() or victim:IsCreature() then
-                    local modifier_death = 'modifier_mjz_lina_laguna_blade_death'
-                    -- ability:ApplyDataDrivenModifier(caster, victim, modifier_death, {duration=kill_grace_duration})
-                    victim:AddNewModifier(caster, ability, modifier_death, {duration=kill_grace_duration})
-                end
+            local damage_type = DAMAGE_TYPE_MAGICAL
+            if caster:HasScepter() then
+                damage_type = DAMAGE_TYPE_PURE
             end
 
             ApplyDamage({
-                ability = self,
                 attacker = caster,
+                victim = target,
                 damage = damage,
-                damage_type = self:GetDamageType(),
-                victim = target
+                damage_type = damage_type,
+                ability = ability,
             })
         end
-        
     end
-
 end
+
+--------------------------------------------------------------------------------
 
 function value_if_scepter(npc, ifYes, ifNot)
 	if npc:HasScepter() then
