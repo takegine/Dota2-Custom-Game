784d260322c99f1222de0d7f6cb8df5ff6d9e5fc
maijz128 <maijz128@gmail.com>
2020-07-02 00:49:51 +0800

    更新英雄技能：拍拍熊 技能：震撼大地 添加位移功能，调整数值

--- Ability/heroes/ursa/ursa_earthshock/game/scripts/vscripts/abilities/hero_ursa/mjz_ursa_earthshock.lua
+++ Ability/heroes/ursa/ursa_earthshock/game/scripts/vscripts/abilities/hero_ursa/mjz_ursa_earthshock.lua
@@ -1,25 +1,52 @@
+LinkLuaModifier("modifier_mjz_ursa_earthshock_movement","abilities/hero_ursa/mjz_ursa_earthshock.lua", LUA_MODIFIER_MOTION_BOTH)
 LinkLuaModifier("modifier_mjz_ursa_earthshock","abilities/hero_ursa/mjz_ursa_earthshock.lua", LUA_MODIFIER_MOTION_NONE)
 LinkLuaModifier("modifier_mjz_ursa_earthshock_talent_radius","abilities/hero_ursa/mjz_ursa_earthshock.lua", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_mjz_ursa_earthshock_talent0_radius","abilities/hero_ursa/mjz_ursa_earthshock.lua", LUA_MODIFIER_MOTION_NONE)
 
 mjz_ursa_earthshock = class({})
 local ability_class = mjz_ursa_earthshock
+
 
 function ability_class:GetAOERadius()
 	local ability = self
 	local caster = self:GetCaster()
 	local base_radius = self:GetSpecialValueFor('shock_radius')
 	local modifier_talent_name = 'modifier_mjz_ursa_earthshock_talent_radius'
+	local modifier_talent0_name = 'modifier_mjz_ursa_earthshock_talent0_radius'
 	local talent_value = 600
+	local talent0_value = 480
 
 	if caster:HasModifier(modifier_talent_name) then
 		return base_radius + talent_value
+	end
+	if caster:HasModifier(modifier_talent0_name) then
+		return base_radius + talent0_value
 	end
 	return base_radius
 end
 
 
+function ability_class:OnSpellStart()
+	if not IsServer() then return end
+	if not self:GetCaster():HasModifier("modifier_mjz_ursa_earthshock_movement") then
+		self:GetCaster():StartGesture(ACT_DOTA_CAST_ABILITY_1)
+		
+		local direction_vector = self:GetCaster():GetForwardVector() * self:GetSpecialValueFor("hop_distance")
+
+		self:GetCaster():AddNewModifier(self:GetCaster(), self, "modifier_mjz_ursa_earthshock_movement", {
+			duration		= self:GetSpecialValueFor("hop_duration"),
+			distance		= self:GetSpecialValueFor("hop_distance"),
+			direction_x		= direction_vector.x,
+			direction_y 	= direction_vector.y,
+			diretion_z 		= direction_vector.z,
+			height			= self:GetSpecialValueFor("hop_height")
+		})
+	end
+end
+
 if IsServer() then
-	function ability_class:OnSpellStart( )
+	
+	function ability_class:ApplyEarthShock( )
 		local ability = self
 		local caster = self:GetCaster()
 		local duration = ability:GetSpecialValueFor('duration')
@@ -64,9 +91,12 @@ if IsServer() then
 
 		local particle_cast = "particles/units/heroes/hero_ursa/ursa_earthshock.vpcf"
 
-		-- get data
 		local slow_radius = GetTalentSpecialValueFor(ability, "shock_radius")
-
+		local slow_radius0 = GetTalentSpecialValueFor(ability, "shock_radius", "LinkedSpecialBonus0")
+		if slow_radius0 > slow_radius then
+			slow_radius = slow_radius0
+		end
+		
 		-- play particles
 		local nFXIndex = ParticleManager:CreateParticle( particle_cast, PATTACH_WORLDORIGIN, nil )
 		ParticleManager:SetParticleControl( nFXIndex, 0, caster:GetOrigin() )
@@ -82,7 +112,9 @@ if IsServer() then
 		local caster = self:GetCaster()
 		local base_radius = self:GetSpecialValueFor('shock_radius')
 		local modifier_talent_name = 'modifier_mjz_ursa_earthshock_talent_radius'
+		local modifier_talent0_name = 'modifier_mjz_ursa_earthshock_talent0_radius'
 		local talent_value = 600
+		local talent0_value = 480
 	
 		if not caster:HasModifier(modifier_talent_name) then
 			local radius = GetTalentSpecialValueFor(ability, "shock_radius")
@@ -90,7 +122,14 @@ if IsServer() then
 				caster:AddNewModifier(caster, ability, modifier_talent_name, {})
 			end
 		end
+		if not caster:HasModifier(modifier_talent0_name) then
+			local radius = GetTalentSpecialValueFor(ability, "shock_radius")
+			if radius > base_radius then
+				caster:AddNewModifier(caster, ability, modifier_talent0_name, {})
+			end
+		end
 	end
+
 end
 
 
@@ -118,10 +157,107 @@ end
 
 -----------------------------------------------------------------------------------------
 
-
 modifier_mjz_ursa_earthshock_talent_radius = class({})
 function modifier_mjz_ursa_earthshock_talent_radius:IsHidden() return true end
 function modifier_mjz_ursa_earthshock_talent_radius:IsPurgable() return false end
+
+-----------------------------------------------------------------------------------------
+
+modifier_mjz_ursa_earthshock_talent0_radius = class({})
+function modifier_mjz_ursa_earthshock_talent0_radius:IsHidden() return true end
+function modifier_mjz_ursa_earthshock_talent0_radius:IsPurgable() return false end
+
+-----------------------------------------------------------------------------------------
+-- https://github.com/EarthSalamander42/dota_imba/blob/master/game/dota_addons/dota_imba_reborn/scripts/vscripts/components/abilities/heroes/hero_ursa.lua
+modifier_mjz_ursa_earthshock_movement = modifier_mjz_ursa_earthshock_movement or class({})
+
+-- Gonna copy-paste my generic motion controller code here cause there's too many changes that need to be made (and I can't copy the class itself for some reason)
+
+function modifier_mjz_ursa_earthshock_movement:IsHidden()		return true end
+function modifier_mjz_ursa_earthshock_movement:IsPurgable()		return false end
+-- function modifier_mjz_ursa_earthshock_movement:GetAttributes()
+-- 	return MODIFIER_ATTRIBUTE_MULTIPLE
+-- end
+
+function modifier_mjz_ursa_earthshock_movement:OnCreated(params)
+	if not IsServer() then return end
+	
+	self.distance			= params.distance
+	self.direction			= Vector(params.direction_x, params.direction_y, params.direction_z):Normalized()
+	self.duration			= params.duration
+	self.height				= params.height
+
+	self.velocity		= self.direction * self.distance / self.duration
+	
+	self.vertical_velocity		= 4 * self.height / self.duration
+	self.vertical_acceleration	= -(8 * self.height) / (self.duration * self.duration)
+	
+	-- Do NOT continue with motion controllers if rooted; trying to be finnicky with this will cause crashes
+	if self:GetParent():IsRooted() then return end
+	
+	if self.distance>0 then
+		if self:ApplyHorizontalMotionController() == false then 
+			self:Destroy()
+			return
+		end
+	end
+	if self.height>=0 then
+		if self:ApplyVerticalMotionController() == false then 
+			self:Destroy()
+			return
+		end
+	end
+end
+
+function modifier_mjz_ursa_earthshock_movement:OnDestroy()
+	if not IsServer() then return end
+	
+	self:GetParent():RemoveHorizontalMotionController(self)
+	self:GetParent():RemoveVerticalMotionController(self)
+	
+	if self:GetAbility() and self:GetAbility().ApplyEarthShock and self:GetRemainingTime() <= 0 then
+		self:GetAbility():ApplyEarthShock()
+	end
+
+	if self.EndCallback then
+		self.EndCallback()
+	end
+	self:GetParent():InterruptMotionControllers( true )
+end
+
+function modifier_mjz_ursa_earthshock_movement:UpdateHorizontalMotion(me, dt)
+	if not IsServer() then return end
+	
+	me:SetAbsOrigin(me:GetAbsOrigin() + self.velocity * dt)
+end
+
+-- This typically gets called if the caster uses a position breaking tool (ex. Blink Dagger) while in mid-motion
+function modifier_mjz_ursa_earthshock_movement:OnHorizontalMotionInterrupted()
+	self:Destroy()
+end
+
+function modifier_mjz_ursa_earthshock_movement:UpdateVerticalMotion(me, dt)
+	if not IsServer() then return end
+	
+	me:SetAbsOrigin(me:GetAbsOrigin() + Vector(0, 0, self.vertical_velocity) * dt)
+
+	self.vertical_velocity = self.vertical_velocity + (self.vertical_acceleration * dt)
+end
+
+-- -- This typically gets called if the caster uses a position breaking tool (ex. Earth Spike) while in mid-motion
+function modifier_mjz_ursa_earthshock_movement:OnVerticalMotionInterrupted()
+	self:Destroy()
+end
+
+function modifier_mjz_ursa_earthshock_movement:OnRefresh( kv )
+end
+function modifier_mjz_ursa_earthshock_movement:OnRemoved()
+end
+
+-- Setter
+function modifier_mjz_ursa_earthshock_movement:SetEndCallback( func ) 
+	self.EndCallback = func
+end
 
 -----------------------------------------------------------------------------------------
 
@@ -134,7 +270,7 @@ function HasTalent(unit, talentName)
 end
 
 -- 获得技能数据中的数据值，如果学习了连接的天赋技能，就返回相加结果
-function GetTalentSpecialValueFor(ability, value)
+function GetTalentSpecialValueFor(ability, value, keyName)
     local base = ability:GetSpecialValueFor(value)
     local talentName
     local kv = ability:GetAbilityKeyValues()
@@ -142,7 +278,7 @@ function GetTalentSpecialValueFor(ability, value)
         if k == "AbilitySpecial" then
             for l,m in pairs(v) do
                 if m[value] then
-                    talentName = m["LinkedSpecialBonus"]
+                    talentName = m[keyName or "LinkedSpecialBonus"]
                 end
             end
         end
